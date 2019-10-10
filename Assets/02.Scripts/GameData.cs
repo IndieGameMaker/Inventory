9eefd2330bc31480c4836a82dc8c7d3868001dca
Jaehyun Lee <myleje@gmail.com>
2019-10-10 16:23:34 +0900

    .

---
+++ Assets/02.Scripts/GameData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DataInfo
+{
+    [Serializable]
+    public class GameData
+    {
+        public string playerName = "";
+        public int level = 1;
+        public float hp = 100.0f;
+        public List<ItemInfo> equipItems = new List<ItemInfo>();
+    }
+
+    [Serializable]
+    public class ItemInfo
+    {
+        public string name;
+        public string desc;
+        public float damage;
+    }
+}
