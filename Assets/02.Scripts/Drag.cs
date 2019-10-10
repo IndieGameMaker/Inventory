db9bfca60aae50b7dc1d72a24316ce073ef03b29
Jaehyun Lee <myleje@gmail.com>
2019-10-10 15:40:14 +0900

    .

--- Assets/02.Scripts/Drag.cs
+++ Assets/02.Scripts/Drag.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Drag : MonoBehaviour, IDragHandler
+public class Drag : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
 {
+    private Transform inventoryTr;
+
     void Start()
     {
-        
+        inventoryTr = GameObject.Find("Inventory").transform;    
     }
 
     public void OnDrag(PointerEventData eventData)
     {
         transform.position = Input.mousePosition;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        transform.SetParent(inventoryTr);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        
     }
 }
