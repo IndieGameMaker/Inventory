18835fd0f4c8e4c0d49e5b87fe29ae4f296207df
Jaehyun Lee <myleje@gmail.com>
2019-10-10 15:36:10 +0900

    .

---
+++ Assets/02.Scripts/Drag.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Drag : MonoBehaviour, IDragHandler
+{
+    void Start()
+    {
+        
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+}
