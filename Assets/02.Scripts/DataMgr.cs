8cb020eab1e86370716b04bfe19660d66e6979e5
Jaehyun Lee <myleje@gmail.com>
2019-10-10 16:31:10 +0900

    .

--- Assets/02.Scripts/DataMgr.cs
+++ Assets/02.Scripts/DataMgr.cs
@@ -1,13 +1,28 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
+using DataInfo;
 
 public class DataMgr : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static DataMgr instance = null;
+    public string dataPath;
+
+    void Awake()
     {
-        
+        if (instance == null)
+        {
+            DontDestroyOnLoad(this.gameObject);
+            instance = this;
+            dataPath = Application.persistentDataPath + "/gameInfo.dat";
+        }
+        else if (instance != this)
+        {
+            Destroy(this.gameObject);
+        }
     }
 
     // Update is called once per frame
