536e30581ca73ee122fea1ae94f71e804ccb2664
Jaehyun Lee <myleje@gmail.com>
2019-10-10 16:51:04 +0900

    .

--- Assets/02.Scripts/UIMgr.cs
+++ Assets/02.Scripts/UIMgr.cs
@@ -1,18 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DataInfo;
 
 public class UIMgr : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void OnSaveClick()
     {
-        
-    }
+        //게임데이터를 저장하기 위한 클래스 생성
+        GameData gameData = new GameData();
+        gameData.playerName = "Zackiller";
+        gameData.level      = 70;
+        gameData.hp         = 1000.0f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        //아이템 생성
+        for (int i=0; i<5; i++)
+        {
+            DataInfo.ItemInfo item = new ItemInfo();
+            item.name = "Item " + i.ToString("00");
+            item.desc = string.Format("Increase {0}% health up", (i+1)*10.0f);
+            item.damage = (i+1)*10.0f;
+
+            gameData.equipItems.Add(item);
+        }
+
+        DataMgr.instance.SaveData(gameData);
     }
 }
